59a24a5bf4c0d2dcabed59df564f6b358fa487ab
ake144 <tamiratgedi1@gmail.com>
2025-12-26 23:16:16 +0300

    feat: initialize NestJS application with basic structure and configuration
    - Added package.json with dependencies and scripts for building, testing, and linting.
    - Created AppController and AppService with a simple "Hello World!" endpoint.
    - Implemented unit tests for AppController.
    - Set up configuration module with environment variable validation using Joi.
    - Added TypeScript configuration files for building and development.
    - Created end-to-end tests for the application.

---
+++ src/config/app.config.ts
@@ -0,0 +1,20 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { configuration } from "./configuration";
+import { validationSchema } from "./vaidation";
+
+const configModule = ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: `../.env`,
+
+    load:[configuration],
+    validationSchema: validationSchema
+});
+
+
+@Module({
+    imports: [configModule],
+    controllers: [],
+})
+
+export class AppConfigModule {}
